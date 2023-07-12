cb7e05aedc85590ae37ffd5fa6b3c3712f416776
unknown <sathiyaibe@gmail.com>
2023-07-12 15:48:14 +0530

    done

--- Assets/JS/style.js
+++ Assets/JS/style.js
@@ -11,20 +11,5 @@ window.addEventListener('scroll', function() {
 
 
 
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    var audio = document.getElementById('my_audio');
-    audio.volume = 0.5; // Adjust the volume as needed
-  
-    // Play the audio when the page loads
-    audio.play();
-  
-    // Pause and reset the audio when the page is about to unload
-    window.addEventListener('beforeunload', function() {
-      audio.pause();
-      audio.currentTime = 0;
-    });
-  });
-})();
 
 
