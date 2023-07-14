c41c074a7dfe11fd0b0f9f79172e1104cffebb7c
unknown <sathiyaibe@gmail.com>
2023-07-14 14:30:07 +0530

    header-updated

--- Assets/JS/style.js
+++ Assets/JS/style.js
@@ -1,9 +1,13 @@
 window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
-    if (window.scrollY > 100) {
+    const element = document.getElementById("pills-tab");
+    if (window.scrollY > 240) {
       header.classList.add('scrolled-header');
+      element.classList.remove("d-none");
     } else {
+     
       header.classList.remove('scrolled-header');
+      element.classList.add("d-none");
     }
   });
 
@@ -13,3 +17,7 @@ window.addEventListener('scroll', function() {
 
 
 
+  function myFunction() {
+    window.scrollTo(0, 910);
+   
+  }
